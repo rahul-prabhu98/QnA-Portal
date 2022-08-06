aef2a61a584145480bfb7c6b20c258aed45dd14d
rahul-prabhu98 <rahulprabhu198@gmail.com>
2022-08-06 14:51:32 +0530

    Enhancement #18 : Task 2 : updating the creations of routes for event requests for Organizer/organizerID and attendee/attendeeID

--- nodeAPI/api/routes/application-route.js
+++ nodeAPI/api/routes/application-route.js
@@ -15,12 +15,51 @@ module.exports = function (expressApp) {
     const userController = require('../controller/user-controller');
     expressApp.route('/signup')
         .post(userController.signup);
+    expressApp.route('/login')
+        .post(userController.login);
+    expressApp.route('/user/changePassword')
+        .post(userController.changePassword);
 
 
     //-- Question
 
+    const questionController = require('../controller/question-controller');
+
+    expressApp.route('/questions')
+        .get(questionController.list)
+        .post(questionController.post);
+
+    /**
+     * Routes '/questions/:id' endpoints to get, Update, Delete a question
+     *
+     * @param 'questions/:id'
+     */
+    expressApp.route('/questions/:questionId')
+        .get(questionController.get)
+        .put(questionController.put)
+        .delete(questionController.delete);
 
     // -- Answer
+
+    expressApp.route('/questions/:questionId/answers')
+        .put(questionController.putAnswer);
+    // -- Tags
+
+    const tagController = require('../controller/tag-controller');
+
+    expressApp.route('/tags')
+        .get(tagController.list)
+        .post(tagController.post);
+
+    /**
+     * Routes '/tags/:tagID' endpoints to get, Update, Delete a tag
+     *
+     * @param 'tags/:tagID'
+     */
+    expressApp.route('/tags/:tagID')
+        .get(tagController.get)
+        .put(tagController.put)
+        .delete(tagController.delete);
 
 
     // -- Meeting/Event
@@ -28,26 +67,7 @@ module.exports = function (expressApp) {
     //const eventController = require('../controller/event-controller');
     //=================================================================================================================================
     const eventRequestController = require('../controller/event-request-controller');
-    /**
-     * Routes '/events/' endpoints to get?p='<field>'&val='<Value>' OR Create a new event
-     *
-     * @param expressApp
-     */
-    // expressApp.route('/events')
-    //     .get(eventController.list)
-    //     .post(eventController.post); //get all events, create a new event
-
-    /**
-     * Routes '/events/:eventID' endpoints to get, Update, Delete an event
-     *
-     * @param 'events/:eventID'
-     */
-    // expressApp.route('/events/:eventID')
-    //     .get(eventController.get)
-    //     .put(eventController.put)
-    //     .delete(eventController.delete);
-
-    /**
+     /**
      * Routes '/event-request' endpoints to Create a new event request towards attendee
      *
      * @param expressApp
@@ -72,9 +92,9 @@ module.exports = function (expressApp) {
      * @param expressApp
      */
     expressApp.route('/event-requests/organizer/:organizerID')
-        .get(eventRequestController.orgGet)
-        .put(eventRequestController.orgPut)
-        .delete(eventRequestController.orgDelete);
+        .get(eventRequestController.orgGet);
+        // .put(eventRequestController.orgPut)
+        // .delete(eventRequestController.orgDelete);
 
 
     /**
@@ -83,25 +103,28 @@ module.exports = function (expressApp) {
      * @param expressApp
      */
     expressApp.route('/event-requests/attendee/:attendeeID')
-        .get(eventRequestController.attGet)
-        .put(eventRequestController.attPut)
-        .delete(eventRequestController.attDelete);
+        .get(eventRequestController.attGet);
+        // .put(eventRequestController.attPut)
+        // .delete(eventRequestController.attDelete);
 
 
 
 
 
 
-    const icsController = require('../controller/ics-generator');
-    /**
-     * Routes '/events/' endpoints to get?p='<field>'&val='<Value>' OR Create a new event
-     *
-     * @param expressApp
-     */
-    expressApp.route('/ics-generator')
-        .get(icsController.list)
-        .post(eventController.post);
+//     const icsController = require('../controller/ics-generator');
+//     /**
+//      * Routes '/events/' endpoints to get?p='<field>'&val='<Value>' OR Create a new event
+//      *
+//      * @param expressApp
+//      */
+//     expressApp.route('/ics-generator')
+//         .get(icsController.list)
+//         .post(eventController.post);
+//
 
+
+    //------------------------------------------------------------
 };
 
 
--- nodeAPI/api/services/event-service.js
+++ nodeAPI/api/services/event-service.js
@@ -8,7 +8,7 @@ const mongoose = require('mongoose');
 /**
  * defining the database/collection for Mongoose object.
  */
-let eventMongoose = mongoose.model('eventDB');
+let eventMongoose = mongoose.model('events');
 
 
 /**
@@ -37,6 +37,24 @@ exports.create = function (eventObj) {
  */
 exports.get = function (eventID) {
     return eventMongoose.findById(eventID).exec();
+};
+
+/**
+ * Returns the event object matching the id.
+ *
+ * @param {string} eventID {Id of the event object}
+ */
+exports.orgGet = function (organizerID) {
+    return eventMongoose.find({organizer : eventID}).exec();
+};
+
+/**
+ * Returns the event object matching the id.
+ *
+ * @param {string} eventID {Id of the event object}
+ */
+exports.attGet = function (attendeeID) {
+    return eventMongoose.find({ attendee: { $all: [ attendeeID ] } }).exec();
 };
 
 /**
